7832bfac3a6cc65b181adf191a5c6e1d1ddb6e4d
mohamadj <mohamad.jarkas@mail.mcgill.ca>
2025-07-14 17:17:13 -0400

    added nvim-autopairs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,21 +1,5 @@
 return {
-  -- nvim-tree plugin setup
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-    config = function()
-      require('nvim-tree').setup {
-        view = {
-          width = 30,
-          side = 'left',
-        },
-        filters = {
-          dotfiles = false,
-        },
-      }
-    end,
-  },
+  -- Load plugins from separate files
+  require 'custom.plugins.nvim-tree',
+  require 'custom.plugins.nvim-autopairs',
 }

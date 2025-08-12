824163f24ecba27c7a0ff865325930cb6799a5fa
mohamadj <mohamad.jarkas@mail.mcgill.ca>
2025-08-12 15:18:04 -0400

    disabled treesitter for cpp, fallback to cinoptions, added indentation for namespace

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,9 @@ vim.o.smartindent = true
 
 vim.cmd [[highlight WinSeparator guifg=black guibg=NONE]]
 
+-- append namespaces in cpp
+vim.opt.cinoptions:append 'Ns'
+
 return {
   require 'custom.plugins.nvim-tree',
   require 'custom.plugins.nvim-autopairs',
